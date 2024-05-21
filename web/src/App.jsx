e4791f3eb2c399c5dd7c70b5f0206a4f32097693
DEN <68790210+DENZEAM@users.noreply.github.com>
2024-05-21 14:48:29 +0200

    chore: Remove commented out code in App.jsx

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -12,12 +12,14 @@ function App() {
 
   return (
     <div
-      style={{
-        backgroundImage:
-          "url('https://media.discordapp.net/attachments/1156492030596358185/1230151777400393858/image.png?ex=6639875e&is=663835de&hm=58cc79d24e08df3d87153f4ce940a64e4a67014cb081300b793f67c566a22a0b&=&format=webp&quality=lossless&width=1258&height=708')",
-        backgroundSize: "cover",
-        backgroundRepeat: "no-repeat",
-      }}
+      style={
+        {
+          // backgroundImage:
+          //   "url('https://media.discordapp.net/attachments/1156492030596358185/1230151777400393858/image.png?ex=6639875e&is=663835de&hm=58cc79d24e08df3d87153f4ce940a64e4a67014cb081300b793f67c566a22a0b&=&format=webp&quality=lossless&width=1258&height=708')",
+          // backgroundSize: "cover",
+          // backgroundRepeat: "no-repeat",
+        }
+      }
     >
       <div style={{ opacity: `${hiddenATH ? "1" : "0"}` }}>
         <ATH></ATH>
