057009c2f6538dc8ea027b873897ef586e669b19
DEN <68790210+DENZEAM@users.noreply.github.com>
2024-05-21 12:52:27 +0200

    chore: Update engine health thresholds in Engin component

--- web/src/views/ATH/components/Engin.jsx
+++ web/src/views/ATH/components/Engin.jsx
@@ -2,8 +2,8 @@ import { Icon } from "@iconify-icon/react";
 
 export const Engin = ({ percentage }) => {
   const GoodHealth = 70;
-  const MediumHealth = 40;
-  const LowHealth = 20;
+  const MediumHealth = 50;
+  const LowHealth = 30;
   return (
     <div
       className={`relative flex flex-col-reverse items-center justify-center w-5 h-5 rounded-sm bg-white/60`}
@@ -15,6 +15,12 @@ export const Engin = ({ percentage }) => {
         ></div>
       )}
       {percentage >= MediumHealth && percentage < GoodHealth && (
+        <div
+          style={{ height: `${percentage}%` }}
+          className={`w-full bg-yellow-500 rounded-sm mt-auto`}
+        ></div>
+      )}
+      {percentage < MediumHealth && percentage > LowHealth && (
         <div
           style={{ height: `${percentage}%` }}
           className={`w-full bg-yellow-500 rounded-sm mt-auto`}
